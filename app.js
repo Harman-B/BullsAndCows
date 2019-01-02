6f78be32ac7fa555cca82e09dd2c2c3b1e5a9fad
Harman Bhatti <hps@jampas.net>
2019-01-02 17:46:52 +0530

    converting the code into prototype functions. All business logics is defined as functions in protypes.

--- app.js
+++ app.js
@@ -1,14 +1,93 @@
-var word;
+function BullsAndCows(word) {
+  this.instance = null;
+  this.word = word;
+  this.hasEnded = false;
+  this.check = null;
+  this.lastGuess = null;
+  this.nbGuesses = 0;
+}
+
+BullsAndCows.prototype.checkGuessWord = function (guessWord) {
+  this.lastGuess = guessWord;
+  nbGuesses++;
+  var check, letterComparisonArr;
+  this.check = {
+    bulls: 0,
+    cows: 0,
+    guesses: 0
+  };
+  letterComparisonArr = [];
+
+  if (guessWord.length === 4) {
+    this.check.guesses++;
+    for (var i = 0; i<4; i++) {
+      letterComparisonArr[i] = guessWord.indexOf(this.word.charAt(i));
+      if (letterComparisonArr[i] === i) {
+        this.check.bulls++;
+      } else if (letterComparisonArr[i] !== -1) {
+        this.check.cows++;
+      }
+    }
+  }
+}
+
+BullsAndCows.prototype.hasGuessedCorrectly = function() {
+  if(this.check == null && this.word !== null) {
+    return false;
+  }
+  if (this.check.bulls === 4) {
+    return true;
+  }
+  return false;
+}
+
+BullsAndCows.prototype.getCurrentStatus = function() {
+  return this.check;
+}
+
+BullsAndCows.prototype.getLastGuess = function() {
+  return this.lastGuess;
+}
+
+BullsAndCows.prototype.create = function(word) {
+  if(this.instance == null) {
+    if(word.length === 4) {
+      this.instance = new BullsAndCows(word);
+    }else {
+      return null;
+    }
+  }
+  return this.instance;
+}
+
+$(document).ready(function(){
+  $("#add_word").click(function(){
+    startGame();
+  });
+  $("#reset").click(function(){
+    resetGame();
+  });
+
+  $("#add_guess").click(function(){
+    makeGuess();
+  });
+
+});
 
 function startGame() {
-  word = getWord();
-  if (word !== undefined) {
+  console.log('Game is about to start.');
+  getWord();
+  if (model !== undefined) {
     document.getElementById('word').value = '';
     document.getElementById('add_word').disabled = true;
     document.getElementById('error').innerText = '';
     document.getElementById('playArea').style.display = 'block';
     document.getElementById('guessWord').focus();
     document.addEventListener('keypress', play);
+    console.log('Game started.');
+
+  }else {
+      console.log('Game did not start.');
   }
 }
 
@@ -26,10 +105,10 @@ function play(event) {
 function makeGuess() {
   var guess, check;
   guess = getGuessWord();
-  check = checkGuessWord(word, guess);
-  displayResult(guess, check);
+  model.checkGuessWord(guess);
+  displayResult();
   clearField();
-  if (check.bulls === 4) {
+  if(model.hasGuessedCorrectly()) {
     endGame();
   }
 }
@@ -42,52 +121,28 @@ function endGame() {
 
 function getWord() {
   var word = document.getElementById('word').value;
-  if (word.length === 4) {
-    return word;
-  } else {
+  model = BullsAndCows.create(word);
+  if (model === null) {
     showError();
-  }
 }
 
 function getGuessWord() {
   return document.getElementById('guessWord').value;
 }
 
-function checkGuessWord(word, guessWord) {
-  var check, letterComparisonArr;
-  check = {
-    bulls: 0,
-    cows: 0,
-    guesses: 0
-  };
-  letterComparisonArr = [];
-
-  if (guessWord.length === 4) {
-    check.guesses++;
-    for (var i = 0; i<4; i++) {
-      letterComparisonArr[i] = guessWord.indexOf(word.charAt(i));
-      if (letterComparisonArr[i] === i) {
-        check.bulls++;
-      } else if (letterComparisonArr[i] !== -1) {
-        check.cows++;
-      }
-    }
-  }
-  return check;
-}
-
 function showError() {
   document.getElementById('error').innerText = 'Enter a four letter word!';
 }
 
-function displayResult(guessWord, check) {
+function displayResult() {
   var html, newHtml, result, el;
 
   el = document.querySelector('.guess_list');
+  var check = model.getCurrentStatus();
   result = check.bulls + 'B' + check.cows + 'C';
 
   html = '<p>%guess%: %result%<p>';
-  newHtml = html.replace('%guess%', guessWord);
+  newHtml = html.replace('%guess%', model.getLastGuess());
   newHtml = newHtml.replace('%result%', result);
 
   el.insertAdjacentHTML('beforeend', newHtml);
